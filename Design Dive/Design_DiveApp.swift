97458b19b20192bdc1aa9d4cfc8b3f63e647a7c4
farizxsidik <farizxsidik@gmail.com>
2024-09-12 16:01:06 +0700

    Initial Commit

---
+++ Design Dive/Design_DiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_DiveApp.swift
+//  Design Dive
+//
+//  Created by Fariz Sidik on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Design_DiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Design DiveUITests/Design_DiveUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Design_DiveUITestsLaunchTests.swift
+//  Design DiveUITests
+//
+//  Created by Fariz Sidik on 12/09/24.
+//
+
+import XCTest
+
+final class Design_DiveUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
